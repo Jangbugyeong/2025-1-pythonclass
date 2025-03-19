7997ac7cb0213b75700a8ecaf5d21f329a71547b
user <bugynite@g.shingu.ac.kr>
2025-03-19 16:07:25 +0900

    다차원 리스트와 조건문

--- 04if.py
+++ 04if.py
@@ -18,8 +18,22 @@ if score>=80:
     grade='B'
 if score >= 70:
     grade = 'c'
-if score >= 60:
-    grade = 'd'
+
 else:
     grade = 'f'
+    print(grade)
+
+    score = int(input("Enter your score: "))
+
+    if score >= 90:
+        grade = 'A'  # 90 이상일 경우 A
+    elif score >= 80:
+        grade = 'B'  # 80 이상일 경우 B
+    elif score >= 70:
+        grade = 'C'  # 70 이상일 경우 C
+    elif score >= 60:
+        grade = 'D'  # 60 이상일 경우 D
+    else:
+        grade = 'F'  # 모든 조건에 만족하지 못할 경우 F
+
     print(grade)
