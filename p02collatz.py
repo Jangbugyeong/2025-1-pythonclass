d8e0a50dcd1bd7cd11ad290679de18870b581e31
user <bugynite@g.shingu.ac.kr>
2025-04-02 16:10:50 +0900

    콜라츠의 추측/프로젝트 2

--- p02collatz.py
+++ p02collatz.py
@@ -1,11 +1,12 @@
 #2025.4.2 파이썬 수업 프로젝트 2
 #콜라츠의 추측, 우박수
+from sys import maxsize
 
 #규칙: n이 짝수라면 n/2 n이 홀수라면 3배+1
 
 i=0
 n=99
-for n in range(1,100):
+for n in range(1,1000):
     i=0
     print(n)
     while n !=1:
@@ -19,6 +20,25 @@ for n in range(1,100):
             print(n)
             i=i+1
     print('반복수:',i)
+
+max_i = 0
+
+for n in range(1, 1000):
+    current_n = n
+    i = 0
+    while current_n != 1:
+        if current_n % 2 == 0:
+            current_n = current_n / 2
+        else:
+            current_n = 3 * current_n + 1
+        i += 1
+
+    if i > max_i:
+        max_i = i
+
+print("i의 최대값:", max_i)
+
+
 
 
 
