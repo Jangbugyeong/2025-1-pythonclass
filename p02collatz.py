4fc454f00abf6ca83594d7156a1df67bc8f1e217
user <bugynite@g.shingu.ac.kr>
2025-04-08 11:44:38 +0900

    2번째로 큰 값 구하기

--- p02collatz.py
+++ p02collatz.py
@@ -1,11 +1,12 @@
 #2025.4.2 파이썬 수업 프로젝트 2
 #콜라츠의 추측, 우박수
+from email.policy import default
 from sys import maxsize
 
 #규칙: n이 짝수라면 n/2 n이 홀수라면 3배+1
 
 i=0
-n=99
+n=97
 for n in range(1,1000):
     i=0
     print(n)
@@ -37,6 +38,37 @@ for n in range(1, 1000):
         max_i = i
 
 print("i의 최대값:", max_i)
+
+maxvalue=0
+maxvaluen=0
+max2value=0
+max2n=0
+for n in range(1,100):
+    print(f'{n=}')
+    ncount=0
+    i=n
+    while i!=1:
+        if i % 2 == 1:
+            i = 3 * i + 1
+        else:
+            i = i / 2
+
+        ncount=ncount+1
+
+    print(f'{ncount}')
+    if ncount > maxvalue:
+        max2value = maxvalue
+        max2n = maxvaluen
+        maxvalue = ncount
+        maxvaluen = n
+    elif ncount > max2value:
+        max2value = ncount
+        max2n = n
+    if maxvalue < ncount:
+        maxvalue = ncount
+        maxvaluen=n
+print(f'{maxvalue=},{maxvaluen=},{max2value=},{max2n=}')
+
 
 
 
