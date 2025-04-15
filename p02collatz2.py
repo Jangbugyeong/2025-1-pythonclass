6c5d434b90909bc56bb75aa46e2c81646c2dcd0f
user <bugynite@g.shingu.ac.kr>
2025-04-15 12:25:16 +0900

    빅3구하기

--- p02collatz2.py
+++ p02collatz2.py
@@ -2,9 +2,16 @@
 #numpy list 두 가지 방식으로 시험
 #구하는 통계량 : 평균,중앙값,표준편차,최빈값,최대값
 import statistics
+from audioop import reverse
+from operator import index
+
 import numpy as np
 import time
-from p02collatzfunc import collatz
+
+from PyQt6.sip import delete
+from numpy.ma.core import argsort
+
+from p02collatzfunc import collatz, find_max,second_largest
 from p02collatz import ncount
 
 MAXNUM=100
@@ -20,12 +27,19 @@ for n in range(1,MAXNUM):
     ncount=collatz(n)
     ncountl.append(ncount)
 
+
+nmax=0
 print(sum(ncountl)/len(ncountl))
 print(f'평균={statistics.mean(ncountl):.5f}')
 print(f'중앙값={statistics.median(ncountl):.5f}')
 #print(f'최빈값={statistics.mode(ncountl):.5f}')
 print(f'표준편차={statistics.pstdev(ncountl):.5f}')
 print(f'최대값={max(ncountl)}')
+print(f'해당숫자={ncountl.index(max(ncountl))+1}')
+print('두 번째로 큰 값 = ',sorted(ncountl, reverse=True)[1])
+print(f'해당 숫자 = {ncountl.index(sorted(ncountl, reverse=True)[1])+1}')
+print('세 번째로 큰 값 = ',sorted(ncountl, reverse=True)[2])
+print(f'해당 숫자 = {ncountl.index(sorted(ncountl, reverse=True)[2])+1}')
 end = time.time()
 print(f'{end-start:.5f} 초가 걸렸습니다.')
 
@@ -36,11 +50,20 @@ ncounta=np.zeros(MAXNUM-1)
 for n in range(1,MAXNUM):
     ncount = collatz(n)
     ncounta[n-1]=ncount
-
+ncounta_sorted=np.argsort(ncounta)
+unique = np.unique(ncounta)
+if unique.size >= 2:
+    second_largest = unique[-2]
+    third_largest = unique[-3]
+print(f'최대값 해당숫자={np.argmax(ncounta)+1}')
 print(f'평균={ncounta.mean():.5f}')
 print(f'중앙값={np.median(ncounta):.5f}')
 print(f'표준편차={np.std(ncounta):.5f}')
 print(f'최대값={np.max(ncounta)}')
+print(f'두 번째로 큰 값 = {second_largest}')
+print(f'해당 숫자 = {ncounta_sorted[-2]+1}')
+print(f'세 번째로 큰 값 = {third_largest}')
+print(f'해당 숫자 = {ncounta_sorted[-3]+1}')
 end2=time.time()
 print(f'{end2-start2:.5f} 초가 걸렸습니다.')
 #print(f'최빈값={np.bincount(ncounta).argmax(ncounta)}')
--- p02collatzfunc.py
+++ p02collatzfunc.py
@@ -1,4 +1,5 @@
 def collatz(i):
+
     ncount=0
     while i!=1:
         if i % 2 == 1:
@@ -7,3 +8,50 @@ def collatz(i):
             i = i / 2
         ncount=ncount+1
     return ncount
+
+def find_max(lst):
+    if not lst:
+        return None  # 빈 리스트일 경우 처리
+    max_value = lst[0]
+    for num in lst[1:]:
+        if num > max_value:
+            max_value = num
+    return max_value
+
+def top_collatz_counts(limit=101):
+    max1_value = max1_n = 0
+    max2_value = max2_n = 0
+    max3_value = max3_n = 0
+
+    for n in range(1, limit):
+        count = 0
+        i = n
+        while i != 1:
+            if i % 2 == 1:
+                i = 3 * i + 1
+            else:
+                i = i / 2
+            count += 1
+
+        if count > max1_value:
+            max3_value, max3_n = max2_value, max2_n
+            max2_value, max2_n = max1_value, max1_n
+            max1_value, max1_n = count, n
+        elif count > max2_value:
+            max3_value, max3_n = max2_value, max2_n
+            max2_value, max2_n = count, n
+        elif count > max3_value:
+            max3_value, max3_n = count, n
+
+    return {
+        '1st': (max1_n, max1_value),
+        '2nd': (max2_n, max2_value),
+        '3rd': (max3_n, max3_value)
+    }
+
+def second_largest(lst):
+    unique = list(set(lst))  # 중복 제거
+    if len(unique) < 2:
+        return None  # 두 번째로 큰 수가 없음
+    unique.sort(reverse=True)
+    return unique[1]
